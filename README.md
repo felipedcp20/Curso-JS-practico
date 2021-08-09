a676b0b54de0b0275c225edc26ae8f7555f7be3d
felipe <felipedcp@gmail.com>
2021-08-09 15:33:01 -0500

    area del cuadrado y inicio del triangulo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Curso-JS-practico
+...
+
+## Taller #1: Figuras Geometricas:
+- Primer Paso: Definir las formulas
+- Segundo paso: implementar las formulas en java script
+- tercer paso : crear funciones
+- cuarto paso : integrar Js en Html
---
+++ figuras.js
@@ -0,0 +1,18 @@
+//Codigo Cuadrado
+const ladoCuadrado = 5;
+console.log(`los lados del cuadrado miden: ${ladoCuadrado}cm`)
+
+const perimetroCuadrado = ladoCuadrado*4;
+console.log(`El perimetro del cuadrado es ${perimetroCuadrado}cm`)
+
+const AreaCuadrado = ladoCuadrado*ladoCuadrado;
+console.log(`El Área del cuadrado es ${AreaCuadrado}cm²`)
+
+//Codigo Triangulo
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const base = 4;
+
+console.log (`los lados del triangulo miden: 
+    ${ladoTriangulo1}cm,${ladoTriangulo2}cm y ${base}cm`);
